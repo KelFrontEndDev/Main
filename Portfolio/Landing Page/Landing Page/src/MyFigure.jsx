9d83044694eeb3f924e3e2d1a72014a1731df6d2
Kelvin Arias <55640509+KelFrontEndDev@users.noreply.github.com>
2024-05-21 17:54:20 -0400

    CSS editing

--- Portfolio/Landing Page/Landing Page/src/MyFigure.jsx
+++ Portfolio/Landing Page/Landing Page/src/MyFigure.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function MyFigure() {
   return (
-    <div>MyFigure</div>
+    <div className='px-44 bg-red-500 flex items-end'>
+      <div className='bg-[#EDA751] w-96 h-96 lg:w-[600px] lg:h-[600px] rounded-full'>
+
+      </div>
+    </div>
   )
 }
 
--- Portfolio/Landing Page/Landing Page/src/MySection.jsx
+++ Portfolio/Landing Page/Landing Page/src/MySection.jsx
@@ -6,7 +6,7 @@ function MySection() {
       <h3 className='text-4xl '>Hello</h3>
       <h1 className='text-6xl font-bold '>I am Kelvin.</h1>
       <h5 className='text-3xl text-[#EDA751]'>Front End Web Developer</h5>
-      <p className='text-[#676767] text-2xl mt-8 mb-20 w-72'>Hello, I am a Front End Web Developer focused in Design and User Experince</p>
+      <p className='text-[#676767] text-2xl mt-8 mb-20 w-80'>Hello, I am a Front End Web Developer focused in Design and User Experince</p>
 
       <button className='bg-[#EDA751] text-white rounded-md px-5 py-2 shadow-md'>Hire Me</button>
     </section>
