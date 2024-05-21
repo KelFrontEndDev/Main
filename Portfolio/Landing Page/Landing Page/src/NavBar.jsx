a7e346fd2563753fae30839f23f7168eab2e19f9
Kelvin Arias <55640509+KelFrontEndDev@users.noreply.github.com>
2024-05-21 14:40:55 -0400

    added Tailwind CSS IntelliSense

--- Portfolio/Landing Page/Landing Page/src/NavBar.jsx
+++ Portfolio/Landing Page/Landing Page/src/NavBar.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <nav className='flex justify-between items-center px-52'>
+      <header className='text'>Kelvin Arias</header>
+      <ul>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+        <li>Home</li>
+        <li>About Me</li>
+        <li>Projects</li>
+        <li>Skills</li>
+        <li>Contact</li>
+      </ul>
+      <button className='outline outline-1 outline-[]'>Resume</button>
+    </nav>
   )
 }
 
