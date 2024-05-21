ab78846d0fdfcd25d68fe39c8cb53ed70461de6e
Kelvin Arias <55640509+KelFrontEndDev@users.noreply.github.com>
2024-05-21 13:39:01 -0400

    Landing Page Commit

--- Portfolio/Landing Page/Landing Page/src/App.js
+++ Portfolio/Landing Page/Landing Page/src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        <a 
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
@@ -18,6 +18,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </div>
   );
 }
