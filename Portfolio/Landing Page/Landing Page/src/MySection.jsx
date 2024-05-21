ca5a9e433bc8b3a1d434b30bb0383d412a18669f
Kelvin Arias <55640509+KelFrontEndDev@users.noreply.github.com>
2024-05-21 17:42:52 -0400

    Finished MySection

--- Portfolio/Landing Page/Landing Page/src/MySection.jsx
+++ Portfolio/Landing Page/Landing Page/src/MySection.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 function MySection() {
   return (
     <section className=' p-44'>
-      <h3 className='text-3xl '>Hello</h3>
-      <h1 className='text-5xl font-bold '>I am Kelvin</h1>
-      <h5 className='text-2xl text-[#EDA751]'>Front End Web Developer</h5>
-      <p className='text-[#676767] mt-8 mb-20 '>Hello, I am a Front End Web Developer focused in Design and User Experince</p>
+      <h3 className='text-4xl '>Hello</h3>
+      <h1 className='text-6xl font-bold '>I am Kelvin.</h1>
+      <h5 className='text-3xl text-[#EDA751]'>Front End Web Developer</h5>
+      <p className='text-[#676767] text-2xl mt-8 mb-20 w-72'>Hello, I am a Front End Web Developer focused in Design and User Experince</p>
 
       <button className='bg-[#EDA751] text-white rounded-md px-5 py-2 shadow-md'>Hire Me</button>
     </section>
