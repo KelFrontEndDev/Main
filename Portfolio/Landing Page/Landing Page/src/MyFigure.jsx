fac8a572ef8e1dcb60a3267f2f2ea7e76df8668f
Kelvin Arias <55640509+KelFrontEndDev@users.noreply.github.com>
2024-05-21 20:34:26 -0400

    Finla Edits

--- Portfolio/Landing Page/Landing Page/src/MyFigure.jsx
+++ Portfolio/Landing Page/Landing Page/src/MyFigure.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import myPic from '../src/Assets/Images/MyProfilePicEnhancedAI-removebg.png'
+
 
 function MyFigure() {
   return (
-    <div className='px-44 bg-red-500 flex items-end'>
-      <div className='bg-[#EDA751] w-96 h-96 lg:w-[600px] lg:h-[600px] rounded-full'>
-
+    <div className='pr-44 flex items-end relative'>
+    <figure>
+        <img className='z-10 fixed bottom-0 right-0 lg:w-[700px] lg:h-800px] ' src={myPic} alt='Pic of Kelvin' />
+      </figure>
+      <div className='bg-[#EDA751] w-96 h-96 lg:w-[1000px] lg:h-[1000px] rounded-full absolute -right-60 -bottom-80 -z-10 '>
       </div>
     </div>
   )
--- Portfolio/Landing Page/Landing Page/src/MySection.jsx
+++ Portfolio/Landing Page/Landing Page/src/MySection.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function MySection() {
   return (
     <section className=' p-44'>
-      <h3 className='text-4xl '>Hello</h3>
+      <h3 className='text-4xl '>Hello,</h3>
       <h1 className='text-6xl font-bold '>I am Kelvin.</h1>
       <h5 className='text-3xl text-[#EDA751]'>Front End Web Developer</h5>
       <p className='text-[#676767] text-2xl mt-8 mb-20 w-80'>Hello, I am a Front End Web Developer focused in Design and User Experince</p>
