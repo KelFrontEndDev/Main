a6170290cf54f581dc224ec5d4987cdf9d1a6185
Kelvin Arias <55640509+KelFrontEndDev@users.noreply.github.com>
2024-05-24 12:29:43 -0400

    Configuration of project

---
+++ Portfolio/to-do-list/src/Components/HeaderToDoForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HeaderToDoForm() {
+  return (
+    <div>HeaderToDoForm</div>
+  )
+}
+
+export default HeaderToDoForm
---
+++ Portfolio/to-do-list/src/Components/ToDoWrapper.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ToDoWrapper() {
+  return (
+    <div>ToDoWrapper</div>
+  )
+}
+
+export default ToDoWrapper
