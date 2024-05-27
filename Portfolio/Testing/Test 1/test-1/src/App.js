508391f6df8dca74254cdad9c840ce55bb308410
Kelvin Arias <55640509+KelFrontEndDev@users.noreply.github.com>
2024-05-27 15:50:44 -0400

    Props Lesson

--- Portfolio/Testing/Test 1/test-1/src/App.js
+++ Portfolio/Testing/Test 1/test-1/src/App.js
@@ -1,1 +1,17 @@
 import './App.css';
+import Student from './Student.js'
+
+
+function App(){
+  return (
+    <>
+      <Student name="Kelvin" age='30' isStudent={true} />
+      <Student name="Patrick" age={42} isStudent={false} />
+      <Student name="bob" age={88} isStudent={false} />
+      <Student name="Sandy" age={18} isStudent={true} />
+      <Student  />
+    </>
+  );
+}
+
+export default  App
