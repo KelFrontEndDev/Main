4e8168a2f2db586e952c1b2324813922a9ed326b
Kelvin Arias <55640509+KelFrontEndDev@users.noreply.github.com>
2024-05-27 12:05:27 -0400

    Props Lesson

--- Portfolio/Testing/Test 1/test-1/src/App.js
+++ Portfolio/Testing/Test 1/test-1/src/App.js
@@ -1,28 +1,20 @@
 import './App.css';
 
-const person = {
-  name: 'Gregorio Y. Zara',
-  theme: {
-    backgroundColor: 'black',
-    color: 'pink'
-  }
-};
-
-export default function TodoList() {
+function Avatar() {
   return (
-    <div style={person.theme}>
-      <h1>{person.name}'s Todos</h1>
-      <img
-        className="avatar"
-        src="https://i.imgur.com/7vQD0fPs.jpg"
-        alt="Gregorio Y. Zara"
-      />
-      <ul>
-        <li>Improve the videophone</li>
-        <li>Prepare aeronautics lectures</li>
-        <li>Work on the alcohol-fuelled engine</li>
-      </ul>
-    </div>
+    <img
+      className="avatar"
+      src="https://i.imgur.com/1bX5QH6.jpg"
+      alt="Lin Lanying"
+      width={100}
+      height={100}
+    />
+  );
+}
+
+export default function Profile() {
+  return (
+    <Avatar />
   );
 }
 
