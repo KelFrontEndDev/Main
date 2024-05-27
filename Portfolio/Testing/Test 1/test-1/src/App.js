1fdfd18932babac692543f2b484b0765c0961e1b
Kelvin Arias <55640509+KelFrontEndDev@users.noreply.github.com>
2024-05-27 12:13:25 -0400

    Erased info

--- Portfolio/Testing/Test 1/test-1/src/App.js
+++ Portfolio/Testing/Test 1/test-1/src/App.js
@@ -1,20 +1,2 @@
 import './App.css';
 
-function Avatar() {
-  return (
-    <img
-      className="avatar"
-      src="https://i.imgur.com/1bX5QH6.jpg"
-      alt="Lin Lanying"
-      width={100}
-      height={100}
-    />
-  );
-}
-
-export default function Profile() {
-  return (
-    <Avatar />
-  );
-}
-
