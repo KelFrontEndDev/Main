f426a63177030e4d8f9186a54ce5b235bb3f640b
Kelvin Arias <55640509+KelFrontEndDev@users.noreply.github.com>
2024-05-28 16:06:55 -0400

    added data to data.js

---
+++ Portfolio/React 25 Projects/accordion/src/components/accordion/data.js
@@ -0,0 +1,32 @@
+const data = [
+    {
+    id : '1',
+    question : 'test?',
+    answer : 'test.',
+    
+    },
+    
+    {
+    id : '2',
+    question : 'test?',
+    answer : 'test.',
+    
+    },
+    
+    {
+    id : '3',
+    question : 'test?',
+    answer : 'test.',
+    
+    },
+    
+    {
+    id : '4',
+    question : 'test?',
+    answer : 'test.',
+    
+    },
+    
+    ];
+
+    export default data;
