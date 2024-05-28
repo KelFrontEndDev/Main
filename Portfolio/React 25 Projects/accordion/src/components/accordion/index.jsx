82cccf84f8f169783bc18e7e771f610785de1959
Kelvin Arias <55640509+KelFrontEndDev@users.noreply.github.com>
2024-05-28 16:21:34 -0400

    Index.js setup

--- Portfolio/React 25 Projects/accordion/src/components/accordion/index.jsx
+++ Portfolio/React 25 Projects/accordion/src/components/accordion/index.jsx
@@ -1,6 +1,7 @@
 //single selection
 
 import { useState } from "react"
+import data from "./data";
 
 // mulitple selection
 export default function Accordion() {
@@ -10,7 +11,15 @@ export default function Accordion() {
     return <div className="wrapper">
         <div className="accordion">
             {
-                data && data.length
+               data && data.length > 0 ?
+               data.map(dataItem=> 
+               <div className="item">
+                <div className="title">
+                    <h3>{dataItem.question}</h3>
+                    <span>+</span>
+                </div>
+               </div>)
+                : <div>No data found!</div>
             }
         </div>
 
