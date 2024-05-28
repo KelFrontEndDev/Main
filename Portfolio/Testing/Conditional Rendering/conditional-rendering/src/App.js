4c8da4b5d548381a653874062ba47ced6e134a6c
Kelvin Arias <55640509+KelFrontEndDev@users.noreply.github.com>
2024-05-27 20:40:22 -0400

    Last Commit for the day

--- Portfolio/Testing/Conditional Rendering/conditional-rendering/src/App.js
+++ Portfolio/Testing/Conditional Rendering/conditional-rendering/src/App.js
@@ -1,53 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
-
-const people = [{
-  id: 0,
-  name: 'Creola Katherine Johnson',
-  profession: 'mathematician',
-}, {
-  id: 1,
-  name: 'Mario José Molina-Pasquel Henríquez',
-  profession: 'chemist',
-}, {
-  id: 2,
-  name: 'Mohammad Abdus Salam',
-  profession: 'physicist',
-}, {
-  id: 3,
-  name: 'Percy Lavon Julian',
-  profession: 'chemist',  
-}, {
-  id: 4,
-  name: 'Subrahmanyan Chandrasekhar',
-  profession: 'astrophysicist',
-}];
-
-const chemists = people.filter(person =>
-  person.profession === 'chemist'
-);
-
-
-
-const listItems = chemists.map(person =>
-  <li>
-     <img
-       src={getImageUrl(person)}
-       alt={person.name}
-     />
-     <p>
-       <b>{person.name}:</b>
-       {' ' + person.profession + ' '}
-       known for {person.accomplishment}
-     </p>
-  </li>
-);
-
-
-
-function App() {
-  return <ul>{listItems}</ul>;
+function Cup({ guest }) {
+  return <h2>Tea cup for guest #{guest}</h2>;
 }
 
-export default App;
+export default function TeaGathering() {
+  let cups = [];
+  for (let i = 1; i <= 12; i++) {
+    cups.push(<Cup key={i} guest={i} />);
+  }
+  return cups;
+}
